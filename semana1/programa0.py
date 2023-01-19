13887adc54bbb120ac9202ad432e55b6e66a6a55
Gustavo-Alderet <122809484+Gustavo-Alderete-Martinez@users.noreply.github.com>
2023-01-19 20:57:30 +0000

    editar el archivo programa0.py

--- semana1/programa0.py
+++ semana1/programa0.py
@@ -1,1 +1,2 @@
-print("Hola mundo en pyton3") # Imprime un mensaje en consola 
+print ("Hola mundo en pyton3") # Imprime un mensaje en consola 
+print ("editado desde replit") # Imprime un mensaje en consola 
